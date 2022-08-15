860aba14139d83cc9eb8ce5681c6090c8fb1095c
Anish-Aby <anishaby963gmail.com>
2022-08-15 13:28:24 +0530

    Add DrakeCommand.

--- src/main/java/commands/DrakeMeme.java
+++ src/main/java/commands/DrakeMeme.java
@@ -1,26 +1,33 @@
 package commands;
 
+import net.dv8tion.jda.api.EmbedBuilder;
 import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
+
+import java.awt.*;
 
 public class DrakeMeme extends ListenerAdapter {
     public void onMessageReceived(MessageReceivedEvent e){
         if(e.getAuthor().isBot()) return;
-
-        String message = e.getMessage().getContentRaw();
+        String message[] = e.getMessage().getContentRaw().split("!");
         String text1 = "";
         String text2 = "";
         String link = "https://api.popcat.xyz/drake?text1=";
+        String query = "";
 
-        if(message.equalsIgnoreCase("$drake")){
-            e.getChannel().sendMessage("Enter text 1: ").queue();
-            e.wait();
-            text1 = e.getMessage().getContentRaw();
-            e.getChannel().sendMessage("Enter text 2: ").queue();
-            text2 = e.getMessage().getContentRaw();
+        if(message[0].equalsIgnoreCase("$drake")){
+            text1 = message[1].replace(" ", "%20");
+            text2 = message[2].replace(" ", "%20");
+            query = text1 + "&text2=" + text2;
+            link += query;
+            e.getMessage().delete().queue();
+            EmbedBuilder eb = new EmbedBuilder();
+            eb.setTitle("ꜰᴀᴄᴛꜱ ʙʏ ᴅʀᴀᴋᴇ");
+            eb.setImage(link);
+            eb.setColor(Color.CYAN);
 
-            link = link + text1 + "&" + "text2=" + text2;
-            e.getChannel().sendMessage(link);
+            e.getChannel().sendMessageEmbeds(eb.build()).queue();
+
         }
     }
 }
